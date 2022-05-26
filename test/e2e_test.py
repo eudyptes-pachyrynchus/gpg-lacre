a63192fad43869ed5deaee29265ccb77294e8eec
eudyptes-pachyrynchus <eudyptes-pachyrynchus@github>
2022-05-26 19:22:40 +0200

    autoformat code

--- test/e2e_test.py
+++ test/e2e_test.py
@@ -1,12 +1,10 @@
 #!/usr/local/bin/python2
 
+import configparser
+import difflib
+import logging
 import os
 import sys
-
-import difflib
-
-import configparser
-import logging
 
 EOL = "\n"
 
@@ -14,6 +12,7 @@ RELAY_SCRIPT = "test/relay.py"
 CONFIG_FILE = "test/gpg-mailgate.conf"
 
 PYTHON_BIN = "python2.7"
+
 
 def build_config(config):
     cp = configparser.ConfigParser()
@@ -32,9 +31,12 @@ def build_config(config):
     cp.set("relay", "host", "localhost")
     cp.set("relay", "port", config["port"])
 
-    logging.debug("Created config with keyhome=%s, cert_path=%s and relay at port %d" %
-                  (config["gpg_keyhome"], config["smime_certpath"], config["port"]))
+    logging.debug(
+        "Created config with keyhome=%s, cert_path=%s and relay at port %d"
+        % (config["gpg_keyhome"], config["smime_certpath"], config["port"])
+    )
     return cp
+
 
 def write_test_config(outfile, **config):
     logging.debug("Generating configuration with %s" % repr(config))
@@ -46,23 +48,27 @@ def write_test_config(outfile, **config):
 
     logging.debug("Wrote configuration to %s" % outfile)
 
-def load_file(name):
-	f = open(name, 'r')
-	contents = f.read()
-	f.close()
 
-	return contents
+def load_file(name):
+    f = open(name, "r")
+    contents = f.read()
+    f.close()
+
+    return contents
+
 
 def strip_eols(strings):
     return [s.strip("\r") for s in strings]
+
 
 def compare(result, expected):
     result_lines = strip_eols(result.split(EOL))
     expected_lines = strip_eols(expected.split(EOL))
 
-    return difflib.unified_diff(expected_lines, result_lines,
-                                fromfile='expected',
-                                tofile='output')
+    return difflib.unified_diff(
+        expected_lines, result_lines, fromfile="expected", tofile="output"
+    )
+
 
 def report_result(message_file, expected_file, test_output):
     expected = load_file(expected_file)
@@ -74,15 +80,20 @@ def report_result(message_file, expected_file, test_output):
     for diff_line in diff:
         print(diff_line)
 
+
 def execute_e2e_test(message_file, expected_file, **kwargs):
-    test_command = "%s gpg-mailgate.py %s < %s" % (PYTHON_BIN, kwargs["from_addr"], message_file)
+    test_command = "%s gpg-mailgate.py %s < %s" % (
+        PYTHON_BIN,
+        kwargs["from_addr"],
+        message_file,
+    )
     result_command = "%s %s %d" % (PYTHON_BIN, RELAY_SCRIPT, kwargs["port"])
 
     logging.debug("Spawning: '%s'" % (result_command))
-    pipe = os.popen(result_command, 'r')
+    pipe = os.popen(result_command, "r")
 
     logging.debug("Spawning: '%s'" % (test_command))
-    msgin = os.popen(test_command, 'w')
+    msgin = os.popen(test_command, "w")
     msgin.write(load_file(message_file))
     msgin.close()
 
@@ -93,6 +104,7 @@ def execute_e2e_test(message_file, expected_file, **kwargs):
 
     report_result(message_file, expected_file, testout)
 
+
 def load_config():
     cp = configparser.ConfigParser()
     cp.read("test/e2e.ini")
@@ -102,19 +114,26 @@ def load_config():
 
 config = load_config()
 
-logging.basicConfig(filename	= "e2e_test.log",
-                    format		= "%(pathname)s:%(lineno)d %(levelname)s [%(funcName)s] %(message)s",
-                    datefmt		= "%Y-%m-%d %H:%M:%S",
-                    level		= logging.DEBUG)
+logging.basicConfig(
+    filename="e2e_test.log",
+    format="%(pathname)s:%(lineno)d %(levelname)s [%(funcName)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.DEBUG,
+)
 
-write_test_config(os.getcwd() + "/" + CONFIG_FILE,
-                  port				= config.getint("relay", "port"),
-                  gpg_keyhome		= "test/keyhome",
-                  smime_certpath	= "test/certs")
+write_test_config(
+    os.getcwd() + "/" + CONFIG_FILE,
+    port=config.getint("relay", "port"),
+    gpg_keyhome="test/keyhome",
+    smime_certpath="test/certs",
+)
 
-for case_no in range(1, config.getint("tests", "cases")+1):
+for case_no in range(1, config.getint("tests", "cases") + 1):
     case_name = "case-%d" % (case_no)
 
-    execute_e2e_test(config.get(case_name, "in"), config.get(case_name, "out"),
-                    from_addr	= config.get(case_name, "from"),
-                    port		= config.getint("relay", "port"))
+    execute_e2e_test(
+        config.get(case_name, "in"),
+        config.get(case_name, "out"),
+        from_addr=config.get(case_name, "from"),
+        port=config.getint("relay", "port"),
+    )
--- test/relay.py
+++ test/relay.py
@@ -8,9 +8,8 @@
 # compare that output with expected clear-text or encrypted message body.
 #
 
-import sys
 import socket
-
+import sys
 
 BUFFER_SIZE = 4096
 EOM = "\r\n.\r\n"
@@ -18,49 +17,54 @@ LAST_LINE = -3
 
 
 def welcome(msg):
-	return "220 %s\r\n" % (msg)
+    return "220 %s\r\n" % (msg)
 
-def ok(msg = "OK"):
-	return "250 %s\r\n" % (msg)
+
+def ok(msg="OK"):
+    return "250 %s\r\n" % (msg)
+
 
 def provide_message():
-	return "354 Enter a message, ending it with a '.' on a line by itself\r\n"
+    return "354 Enter a message, ending it with a '.' on a line by itself\r\n"
+
 
 def receive_and_confirm(session):
-	session.recv(BUFFER_SIZE)
-	session.sendall(ok())
+    session.recv(BUFFER_SIZE)
+    session.sendall(ok())
+
 
 def serve(port):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('', port))
-	s.listen(1)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", port))
+    s.listen(1)
 
-	(conn, addr) = s.accept()
-	conn.sendall(welcome("TEST SERVER"))
+    (conn, addr) = s.accept()
+    conn.sendall(welcome("TEST SERVER"))
 
-	receive_and_confirm(conn)				# Ignore HELO/EHLO
-	receive_and_confirm(conn)				# Ignore sender address
-	receive_and_confirm(conn)				# Ignore recipient address
+    receive_and_confirm(conn)  # Ignore HELO/EHLO
+    receive_and_confirm(conn)  # Ignore sender address
+    receive_and_confirm(conn)  # Ignore recipient address
 
-	data = conn.recv(BUFFER_SIZE)
-	conn.sendall(provide_message())
+    data = conn.recv(BUFFER_SIZE)
+    conn.sendall(provide_message())
 
-	# Consume until we get <CR><LF>.<CR><LF>, the end-of-message marker.
-	message = ''
-	while not message.endswith(EOM):
-		message += conn.recv(BUFFER_SIZE)
-	conn.sendall(ok("OK, id=test"))
+    # Consume until we get <CR><LF>.<CR><LF>, the end-of-message marker.
+    message = ""
+    while not message.endswith(EOM):
+        message += conn.recv(BUFFER_SIZE)
+    conn.sendall(ok("OK, id=test"))
 
-	# Trim EOM marker as we're only interested in the message body.
-	return message[:-len(EOM)]
+    # Trim EOM marker as we're only interested in the message body.
+    return message[: -len(EOM)]
+
 
 def error(msg):
-	print("ERROR: %s" % (msg))
-	sys.exit(1)
+    print("ERROR: %s" % (msg))
+    sys.exit(1)
 
 
 if len(sys.argv) < 2:
-	error("Usage: relay.py PORT_NUMBER")
+    error("Usage: relay.py PORT_NUMBER")
 
 port = int(sys.argv[1])
 body = serve(port)
